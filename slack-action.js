440d548476aef357ca70ab1470d4d7cffcbf7eaa
aleksandr.bulychev <aleksandr.bulychev@devexpress.com>
2022-10-14 19:44:55 +0400

    CodeQL research

--- slack-action.js
+++ slack-action.js
@@ -1,8 +1,49 @@
 const core = require('@actions/core');
+const { IncomingWebhook } = require('@slack/webhook');
+
+const webhook = new IncomingWebhook('https://hooks.slack.com/services/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');
 
 async function run() {
     try {
-        const alerts = core.getInput('alerts', { required: true });
+        const alerts = JSON.parse(core.getInput('alerts', { required: true }));
+        const commit_oid = core.getInput('commit_oid', { required: true });
+
+        for (let alert of alerts) {
+            if (alert.state === 'open') {
+                await webhook.send({
+                    text: `A Code Scanning alert from ${alert.tool.name} has just been found and created. Information about the alert can be found below.`,
+                    attachments: [
+                        {
+                            color: "danger",
+                            title: `${alert.rule.id}`,
+                            title_link: `${alert.html_url}`,
+                            fields: [
+                                {
+                                    title: "Rule ID",
+                                    value: `${alert.rule.id}`,
+                                    short: true,
+                                },
+                                {
+                                    title: "Rule Description",
+                                    value: `${alert.rule.description}`,
+                                    short: true,
+                                },
+                                {
+                                    title: "Alert Severity:",
+                                    value: `${alert.rule.severity}`,
+                                    short: true,
+                                },
+                                {
+                                    title: "Commit Found In:",
+                                    value: `${commit_oid}`,
+                                    short: true,
+                                },
+                            ],
+                        },
+                    ]
+                });
+            }
+        }
 
         console.log(alerts);
     } catch (error) {
